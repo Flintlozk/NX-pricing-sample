7990b3bc8c26f03fceafa82d431b1f67badacfb6
Flintlozk <apithana@theiconweb.com>
2022-08-01 16:01:49 +0700

    Add product

--- apps/pricing-sample-backend/src/graphql/index.ts
+++ apps/pricing-sample-backend/src/graphql/index.ts
@@ -1,10 +1,12 @@
 import {
   CommonTypeDefs,
   PriceTypeDefs,
+  ProductTypeDefs,
 } from '@pricing-sample-nx/shared-models';
 import { commonResolver } from '../controllers/common/common.controller';
 import { merge } from 'lodash';
 import { priceResolver } from '../controllers/price/price.controller';
+import { productResolver } from '../controllers/product/product.controller';
 
-export const typeDefs = [CommonTypeDefs, PriceTypeDefs];
-export const resolvers = merge(commonResolver, priceResolver);
+export const typeDefs = [CommonTypeDefs, PriceTypeDefs, ProductTypeDefs];
+export const resolvers = merge(commonResolver, priceResolver, productResolver);
